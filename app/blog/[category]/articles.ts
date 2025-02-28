cd52b5668d94b790ef20052324ac774df946efa2
Dahlin301 <jakob.dahlin@gmail.com>
2025-02-27 21:09:49 -0500

    B/W article added

--- app/blog/[category]/articles.ts
+++ app/blog/[category]/articles.ts
@@ -1,5 +1,6 @@
 import RealEstateVsArchitectural from "@/app/blog/articles/real-estate-vs-architectural-photography";
 import RockefellerCenterPhotography from "@/app/blog/articles/rockefeller-center-photography";
+import BlackAndWhitePhotographyNYC from "@/app/blog/articles/black-and-white-photography-in-new-york-city";
 
 
 export const articles = [
@@ -18,5 +19,13 @@ export const articles = [
     coverImage: "https://res.cloudinary.com/dj10sb6gx/image/upload/v1739500374/JD_2342_1_oy6bh8.jpg",
     component: RockefellerCenterPhotography,
     category: "tutorials-tips",
+  },
+  {
+    title: "Black and white photography in New York City",
+    slug: "black-and-white-photography-in-new-york-city",
+    excerpt: "Black and white photography in New York City captures the monochrome essence of the metropolis in a way that color images often...",
+    coverImage: "https://res.cloudinary.com/dj10sb6gx/image/upload/v1740705840/IMG_2100_5x7empty_xlmb9j.jpg",
+    component: BlackAndWhitePhotographyNYC,
+    category: "technical-mastery",
   },
 ];
